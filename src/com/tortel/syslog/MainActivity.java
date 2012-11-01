4bb754d17022d017073131aee379da31a85a8b4e
Scott Warner <Tortel1210@gmail.com>
2012-11-01 15:27:07 -0400

    Saves and compresses logs

--- src/com/tortel/syslog/MainActivity.java
+++ src/com/tortel/syslog/MainActivity.java
@@ -1,5 +1,10 @@
 package com.tortel.syslog;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.SharedPreferences;
@@ -7,6 +12,7 @@ import android.content.SharedPreferences.Editor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
@@ -37,6 +43,11 @@ public class MainActivity extends Activity {
 		box = (CheckBox) findViewById(R.id.modem_log);
 		box.setChecked(modemLog);
 		shell = new Shell();
+		
+		//Check for root access
+		if(!shell.root()){
+			Toast.makeText(this, R.string.noroot, Toast.LENGTH_LONG).show();
+		}
 	}
 	
 	
@@ -103,13 +114,49 @@ public class MainActivity extends Activity {
 		 * Process the logs
 		 */
 		protected Boolean doInBackground(Void... params) {
-			
-			return null;
+			if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+				//Create the directories
+			    String path = Environment.getExternalStorageDirectory().getPath();
+			    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH.mm", Locale.US);
+			    Date date = new Date();
+				path += "/SysLog/"+sdf.format(date)+"/";
+			    File outPath = new File(path);
+			    Log.v(Shell.TAG, "Path: "+path);
+			    if(!outPath.mkdirs()){
+			    	return false;
+			    }
+			    
+			    //Dump the logs
+			    if(kernelLog){
+			    	shell.exec("dmesg > "+path+"dmesg.log");
+			    }
+			    if(mainLog){
+			    	shell.exec("logcat -v time -d -f "+path+"logcat.log");
+			    }
+			    if(modemLog){
+			    	shell.exec("logcat -v time -b radio -d -f "+path+"modem.log");
+			    }
+			    
+			    //Change directory
+			    shell.exec("cd "+path);
+			    
+			    //Compress them
+			    shell.exec("tar -cf "+path+"logs.tar *");
+			    
+			    //TODO: Figure out how to create a share dialog
+			    
+				return true;
+			}
+			return false;
 		}
 		
 		protected void onPostExecute(Boolean result){
 			dialog.dismiss();
-			Toast.makeText(getBaseContext(), "Done!", Toast.LENGTH_SHORT).show();
+			if(result){
+				Toast.makeText(getBaseContext(), R.string.done, Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(getBaseContext(), R.string.error, Toast.LENGTH_LONG).show();
+			}
 		}
 		
 	}
