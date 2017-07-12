cd7050f59dadbad68df79110828dfbf12208ff61
Scott Warner <Tortel1210@gmail.com>
2017-07-12 13:37:48 -0400

    Fix grabbing logs on N+
    As of Android N, sharing file:// URIs is not allowed, instead switch to the FileProvider

--- app/src/main/java/com/tortel/syslog/dialog/RunningDialog.java
+++ app/src/main/java/com/tortel/syslog/dialog/RunningDialog.java
@@ -2,11 +2,11 @@ package com.tortel.syslog.dialog;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.FileProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +20,8 @@ import com.tortel.syslog.utils.*;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.File;
 
 /**
  * Fragment which actually runs the commands
@@ -85,10 +87,13 @@ public class RunningDialog extends DialogFragment {
                 Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
 
                 //Display a share intent
+                File zipFile = new File(result.getArchivePath());
+
                 Intent share = new Intent(android.content.Intent.ACTION_SEND);
                 share.setType("application/zip");
                 share.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                share.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + result.getArchivePath()));
+                share.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(getActivity(),
+                        "com.tortel.syslog.fileprovider", zipFile));
 
                 if (Utils.isHandlerAvailable(getActivity().getApplicationContext(), share)) {
                     startActivity(share);
